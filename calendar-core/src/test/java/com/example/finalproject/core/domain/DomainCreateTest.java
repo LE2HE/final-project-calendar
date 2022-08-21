e7662cc4af0a6fd0811092939275f40cf56295b9
LE2HE <leehe4352@gamil.com>
2022-08-21 17:37:00 +0900

    Test #3 : 도메인 테스트
    User, Event 도메인 테스트

---
+++ calendar-core/src/test/java/com/example/finalproject/core/domain/DomainCreateTest.java
@@ -0,0 +1,38 @@
+package com.example.finalproject.core.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DomainCreateTest {
+    @Test
+    void eventCreate() {
+        final User writer = new User(
+                "writer",
+                "email@email",
+                "pw",
+                LocalDate.now(),
+                LocalDateTime.now());
+
+        final User attendee = new User(
+                "attendee",
+                "email@email",
+                "pw",
+                LocalDate.now(),
+                LocalDateTime.now());
+
+        final Event event = new Event(
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                "title",
+                "desc",
+                writer,
+                LocalDateTime.now());
+
+        event.addEngagement(new Engagement(event, attendee, LocalDateTime.now(), RequestStatus.REQUESTED));
+        assertEquals(event.getEngagements().get(0).getEvent().getWriter().getName(), "writer");
+    }
+}
