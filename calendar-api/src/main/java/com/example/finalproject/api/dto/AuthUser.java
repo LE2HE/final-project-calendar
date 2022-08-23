61e2778a920543e5b7bf57a885d5c4a2add3561c
LE2HE <leehe4352@gamil.com>
2022-08-23 14:36:42 +0900

    Feat #15 : Auth User 정의
    AuthUser (Getter)
    - id
    - of : Id 매핑

---
+++ calendar-api/src/main/java/com/example/finalproject/api/dto/AuthUser.java
@@ -0,0 +1,18 @@
+package com.example.finalproject.api.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AuthUser {
+
+    private final Long id;
+
+    private AuthUser(Long id) {
+        this.id = id;
+    }
+
+    public static AuthUser of(Long id) {
+        return new AuthUser(id);
+    }
+
+}
