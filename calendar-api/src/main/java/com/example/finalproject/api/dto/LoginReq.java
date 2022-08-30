544b6f1fb6891a1587f9d284f955f58d468edfeb
LE2HE <leehe4352@gamil.com>
2022-08-30 23:56:32 +0900

    Feat #29 : Login Validation 기능 추가
    LoginReq
    - email
    - password : `@Size` 추가

--- calendar-api/src/main/java/com/example/finalproject/api/dto/LoginReq.java
+++ calendar-api/src/main/java/com/example/finalproject/api/dto/LoginReq.java
@@ -3,6 +3,7 @@ package com.example.finalproject.api.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class LoginReq {
@@ -10,6 +11,7 @@ public class LoginReq {
     @NotBlank
     private final String email;
 
+    @Size(min = 6, message = "6자리 이상 입력해주세요.")
     @NotBlank
     private final String password;
 
