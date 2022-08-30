4b2e84cb42ffc899e304fb1c40af3e4e4761fb9a
LE2HE <leehe4352@gamil.com>
2022-08-30 23:47:21 +0900

    Feat #29 : Exception 변경
    Calendar Exception 으로 변경

--- calendar-api/src/main/java/com/example/finalproject/api/service/EventService.java
+++ calendar-api/src/main/java/com/example/finalproject/api/service/EventService.java
@@ -9,6 +9,8 @@ import com.example.finalproject.core.domain.entity.User;
 import com.example.finalproject.core.domain.entity.repository.EngagementRepository;
 import com.example.finalproject.core.domain.entity.repository.ScheduleRepository;
 import com.example.finalproject.core.domain.entity.repository.UserRepository;
+import com.example.finalproject.core.exception.CalendarException;
+import com.example.finalproject.core.exception.ErrorCode;
 import com.example.finalproject.core.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -34,7 +36,7 @@ public class EventService {
                         && e.getRequestStatus() == RequestStatus.ACCEPTED
                         && e.getEvent().isOverlapped(eventCreateReq.getStartAt(), eventCreateReq.getEndAt()))
         ) {
-            throw new RuntimeException("cannot make engagement. period overlapped!");
+            throw new CalendarException(ErrorCode.EVENT_CREATE_OVERLAPPED_PERIOD);
         }
         final Schedule eventSchedule = Schedule.event(
                 eventCreateReq.getTitle(),
